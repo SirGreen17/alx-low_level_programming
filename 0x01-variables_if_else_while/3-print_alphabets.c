b0f6531d659e485327172ad8aabb77edc5779f6b
Osemeke89 <Osemeke89@gmail.com>
2022-09-12 19:38:15 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Alphabets in uppercase
+ *
+ * Return: Always 1 (Success) 
+ */
+
+int main(void)
+{
+	int A = 65;
+	int B = 97;
+
+	while (B < 123)
+	{
+		putchar(B);
+		B++;
+	}
+	while (A < 91)
+	{
+		putchar(A);
+		A++;
+	}
+	putchar(10);
+	return(0);
+}
