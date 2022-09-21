01e8e373c12ea937930b9ddfb7cc3d567992e864
Osemeke Onyekachukwu <52927146+Osemeke89@users.noreply.github.com>
2022-09-21 03:42:43 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  _puts - prints a string to standard output
+ *  @str: a string
+ *
+ *  Return: void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
