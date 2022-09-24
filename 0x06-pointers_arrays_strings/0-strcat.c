df9ae1418391dfda6bea568f0b1a4ff9487af05d
Osemeke Onyekachukwu <52927146+Osemeke89@users.noreply.github.com>
2022-09-24 03:24:30 +0100

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate 2 strings
+ * _strcat - concatenates 2 strings
  * @dest: destination
  * @src: source
  *
  * Return: 0
  */
 
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i = 0;
+	int n = 0;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	while (*(dest + i))
+		i++;
+	while ((*(dest + i) = *(src + n)))
+	{
+		i++;
+		n++;
+	}
 	return (dest);
 }
